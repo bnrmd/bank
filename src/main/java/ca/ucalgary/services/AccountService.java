51def46ef8e94ce713525e69ac4cc8fe019c94b1
Ben Raymond <ben.ray.com@gmail.com>
2019-02-09 21:49:03 -0700

    Adding Comments

--- src/main/java/ca/ucalgary/services/AccountService.java
+++ src/main/java/ca/ucalgary/services/AccountService.java
@@ -1,4 +1,16 @@
 package ca.ucalgary.services;
 
+import ca.ucalgary.domain.Account;
+
+import java.util.UUID;
+
 public class AccountService {
+    public Account deposit(UUID accountId, double amount){
+        // Step 1: Fetch the account POJO from datastore
+        // Step 2: Create a new account transaction of type deposit
+        // Step 3: Update the account POJO amount balance
+        // Step 4: Save the account balance back into the datastore
+        // Step 5: Update the account transaction status to success or failure
+        // Step 6: Save the account transaction to the datastore
+    }
 }
