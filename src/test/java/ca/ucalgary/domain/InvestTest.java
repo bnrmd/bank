0f208b9523dd9a118c25075bb6838e99a2d8d51d
Salim <47361425+Salim21@users.noreply.github.com>
2019-03-11 17:23:58 -0600

    Created InvestTest

---
+++ src/test/java/ca/ucalgary/domain/InvestTest.java
@@ -0,0 +1,34 @@
+package ca.ucalgary.domain;
+
+import static org.junit.Assert.assertEquals;
+
+// Imports
+import org.junit.Test;
+import ca.ucalgary.domain.Invest;
+import java.util.ArrayList;
+
+/**
+ * InvestTest Class
+ * test Invest class
+ */
+public class InvestTest {
+	
+	@Test
+	public void test_addStock_addRY100() {
+		
+		// test setup
+		Invest myInvest = new Invest();
+		
+		// run test
+		myInvest.addStock("RY", 100);
+		
+		// verify
+		ArrayList<String> actualValue = new ArrayList<String>();
+		actualValue.add("RY");
+		actualValue.add(String.valueOf(100));
+		
+		assertEquals("100 shares of RY", myInvest.getMyStoks(), actualValue);
+		
+	}
+
+}
