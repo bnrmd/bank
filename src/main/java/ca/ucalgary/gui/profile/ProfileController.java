e0ae8d556e99218a11f3c16b148ed3dab528b204
Ben Raymond <ben.ray.com@gmail.com>
2019-03-25 15:49:52 -0700

    Adding Profile Feature

---
+++ src/main/java/ca/ucalgary/gui/profile/ProfileController.java
@@ -0,0 +1,37 @@
+package ca.ucalgary.gui.profile;
+
+import ca.ucalgary.domain.Customer;
+import ca.ucalgary.services.AccountService;
+import ca.ucalgary.services.CustomerService;
+import ca.ucalgary.services.RepositoryService;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ProfileController implements Initializable {
+    private Customer customer;
+    @FXML
+    private AnchorPane main;
+    public Label name;
+    public Label fname;
+    public Label lname;
+    public Label clientId;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) { }
+
+    @FXML
+    public void setProfile(Customer c){
+        this.fname.setText(c.getFirstName());
+        this.lname.setText(c.getLastName());
+        this.clientId.setText(c.getEmail());
+    }
+
+    public void setCustomer(Customer customer){
+        this.customer = customer;
+    }
+}
